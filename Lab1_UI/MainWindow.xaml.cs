46cee41a528c4011c4a239cebb1ed8c343e69d36
markvrc <markvirchenko@gmail.com>
2020-04-11 13:17:49 +0300

    refactored duplicate logic for encrypt & decrypt buttons

--- Lab1_UI/MainWindow.xaml.cs
+++ Lab1_UI/MainWindow.xaml.cs
@@ -27,28 +27,20 @@ namespace Lab1_UI
             InitializeComponent();
         }
 
-        private async void Decrypt_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            var text = tbText.Text;
+            var isDecryption = sender == Decrypt;
+
+            var text = isDecryption ? tbText.Text : formatting.textToBinary(tbText.Text);
             var keyText = tbKey.Text;
-            string key = await Task.Run( () => RawTextToSHAkey(keyText) );
-            var isDecryption = true;
-            tbOutput.Text = await Task.Run( () => GetResult(text, key, isDecryption) );
+            string key = await Task.Run(() => RawTextToSHAkey(keyText));            
+            tbOutput.Text = await Task.Run(() => GetResult(text, key, isDecryption));
         }
 
         private string RawTextToSHAkey(string text)
         {
             return String.Concat(System.Security.Cryptography.SHA256.Create().ComputeHash(Encoding.Unicode.GetBytes(text)).Take(64).Select(i => Convert.ToString(i, 2)));
-        }
-        
-        private async void Encrypt_Click(object sender, RoutedEventArgs e)
-        {
-            var text = formatting.textToBinary(tbText.Text);
-            var keyText = tbKey.Text;
-            string key = await Task.Run(() => RawTextToSHAkey(keyText));
-            var isDecryption = false;
-            tbOutput.Text = await Task.Run(() => GetResult(text, key, isDecryption));
-        }
+        }       
 
         private string GetResult(IEnumerable<char> text, string key, bool isDecryption)
         {
